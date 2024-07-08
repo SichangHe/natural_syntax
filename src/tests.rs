46f6370081f8ebeaa5f946b4ef4a27feebfc1e31
Steven Hé (Sīchàng) <stevensichanghe@gmail.com>
2024-07-08 12:48:15 +0800

    chore(clean up): extract prediction into `POSModel`

--- src/tests.rs
+++ src/tests.rs
@@ -7,17 +7,12 @@ use insta::assert_debug_snapshot;
 #[test]
 fn paragraph_pos() {
     let start = Instant::now();
-    let model = pos_model().unwrap();
+    let model = POSModel::try_default().unwrap();
     println!("Took {}ms to load the model.", start.elapsed().as_millis());
-    let input = ["Extracts Part of Speech tags (Noun, Verb, Adjective…) from text. A lightweight pretrained model using MobileBERT is available for English."];
+    let input = "Extracts Part of Speech tags (Noun, Verb, Adjective…) from text. A lightweight pretrained model using MobileBERT is available for English.";
     let start = Instant::now();
-    let mut output = model.predict(&input, true, false);
+    let output = model.predict(input);
     println!("Took {}ms to predict.", start.elapsed().as_millis());
-    assert_eq!(1, output.len());
-    let parsed = output[0]
-        .drain(..)
-        .map(POSToken::try_from)
-        .collect::<Result<Vec<_>, _>>()
-        .unwrap();
+    let parsed = output.collect::<Result<Vec<_>, _>>().unwrap();
     assert_debug_snapshot!(parsed);
 }
