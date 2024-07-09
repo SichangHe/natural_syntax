f886a2b787b0b7c496d29b1e164f33adf5f6a6e3
Steven Hé (Sīchàng) <stevensichanghe@gmail.com>
2024-07-09 22:30:15 +0800

    feat(semantic_token): LS parse document&provide tokens feat(trait): mark `POSModel` `Send` and `Sync`

--- natural_syntax_ls/src/lib.rs
+++ natural_syntax_ls/src/lib.rs
@@ -1,18 +1,94 @@
+use std::sync::Arc;
+
+use dashmap::DashMap;
+use natural_syntax::{POSModel, POSToken};
+use ropey::Rope;
+use tokio::task::{block_in_place, yield_now};
+use tokio_two_join_set::TwoJoinSet;
 use tower_lsp::{jsonrpc::Result, lsp_types::*, Client, LanguageServer};
 
 mod semantic_tokens;
 
 use semantic_tokens::*;
+use tracing::{error, info};
 
-#[derive(Debug)]
+pub type Documents = Arc<DashMap<Url, Document>>;
+
 pub struct POSLS {
     client: Client,
+    model: Arc<POSModel>,
+    documents: Documents,
+    prediction_join_sets: DashMap<Url, TwoJoinSet<()>>,
 }
 
+const TOKEN_SCORE_THRESHOLD: f64 = 1. / 3.;
+
 impl POSLS {
-    pub fn new(client: Client) -> Self {
-        Self { client }
+    pub fn new(client: Client, model: POSModel) -> Self {
+        Self {
+            client,
+            model: Arc::new(model),
+            documents: Default::default(),
+            prediction_join_sets: Default::default(),
+        }
     }
+
+    async fn on_change(&self, item: TextDocumentItem) {
+        let uri = item.uri.clone();
+        let task = process_document(item, self.model.clone(), self.documents.clone());
+        _ = self
+            .prediction_join_sets
+            .entry(uri)
+            .or_default()
+            .spawn(task);
+    }
+}
+
+async fn process_document(
+    TextDocumentItem { uri, text, version }: TextDocumentItem,
+    model: Arc<POSModel>,
+    documents: Documents,
+) {
+    if block_in_place(|| version_outdated(&documents, &uri, version)) {
+        return info!(?uri, version, "Ignoring outdated version.");
+    }
+    yield_now().await;
+    let predictions = block_in_place(|| model.predict(&text));
+    yield_now().await;
+    let tokens = predictions
+        .filter_map(|maybe_token| match maybe_token {
+            Ok(token) => Some(token),
+            Err(err) => {
+                error!(?err, ?uri, "Tagging text.");
+                None
+            }
+        })
+        .filter(filter_token)
+        .collect();
+    if block_in_place(|| version_outdated(&documents, &uri, version)) {
+        return info!(
+            ?uri,
+            version, "Ignoring outdated version after classifying."
+        );
+    }
+    yield_now().await;
+    let document = Document {
+        text: Rope::from_str(&text),
+        tokens,
+        version,
+    };
+    yield_now().await;
+    block_in_place(|| documents.insert(uri, document));
+}
+
+fn version_outdated(documents: &DashMap<Url, Document>, uri: &Url, version: i32) -> bool {
+    matches!(documents.get(uri), Some(doc) if version <= doc.version)
+}
+
+/// Filter out tokens with low score or purely punctuations.
+pub fn filter_token(token: &POSToken) -> bool {
+    token.score > TOKEN_SCORE_THRESHOLD
+        && token.word.chars().any(|char| !char.is_ascii_punctuation())
 }
 
 #[tower_lsp::async_trait]
@@ -24,6 +100,45 @@ impl LanguageServer for POSLS {
         })
     }
 
+    async fn did_open(&self, params: DidOpenTextDocumentParams) {
+        self.on_change(TextDocumentItem {
+            uri: params.text_document.uri,
+            text: params.text_document.text,
+            version: params.text_document.version,
+        })
+        .await
+    }
+
+    async fn did_change(&self, mut params: DidChangeTextDocumentParams) {
+        // TODO: Partial changes.
+        debug_assert_eq!(
+            params.content_changes.len(),
+            1,
+            "We only take full changes."
+        );
+        self.on_change(TextDocumentItem {
+            uri: params.text_document.uri,
+            text: params.content_changes.pop().unwrap().text,
+            version: params.text_document.version,
+        })
+        .await
+    }
+
+    async fn semantic_tokens_full(
+        &self,
+        params: SemanticTokensParams,
+    ) -> Result<Option<SemanticTokensResult>> {
+        let Some(document) = block_in_place(|| self.documents.get(&params.text_document.uri))
+        else {
+            return Ok(None);
+        };
+        let (text, tokens) = (&document.text, &document.tokens);
+        Ok(Some(SemanticTokensResult::Tokens(SemanticTokens {
+            result_id: None,
+            data: semantic_tokens(text, tokens),
+        })))
+    }
+
     async fn shutdown(&self) -> Result<()> {
         self.client // Placeholder to make `self.client` used.
             .log_message(MessageType::WARNING, "Exiting.")
@@ -32,22 +147,66 @@ impl LanguageServer for POSLS {
     }
 }
 
+fn semantic_tokens(text: &Rope, tokens: &[POSToken]) -> Vec<SemanticToken> {
+    let mut i_prev_start = 0;
+    let mut slice = text.slice(..);
+    tokens
+        .iter()
+        .map(|token| {
+            let relative_i_char = token.offset_begin - i_prev_start;
+            let relative_i_line = slice.char_to_line(relative_i_char as usize);
+            let delta_start = match relative_i_line {
+                0 => relative_i_char,
+                _ => {
+                    let i_1st_char_of_line = slice.line_to_char(relative_i_line);
+                    relative_i_char - i_1st_char_of_line as u32
+                }
+            };
+            i_prev_start = token.offset_begin;
+            slice = slice.slice((relative_i_char as usize)..);
+            let type_n_modifiers = token_type_n_modifiers(token.tag);
+            SemanticToken {
+                delta_line: relative_i_line as u32,
+                delta_start,
+                length: token.offset_end - token.offset_begin,
+                token_type: type_n_modifiers.token_type,
+                token_modifiers_bitset: type_n_modifiers.token_modifiers_bitset,
+            }
+        })
+        .collect()
+}
+
+#[derive(Clone, Debug)]
+struct TextDocumentItem {
+    uri: Url,
+    text: String,
+    version: i32,
+}
+
 fn server_capabilities() -> ServerCapabilities {
     ServerCapabilities {
         // TODO: Implement incremental change.
         text_document_sync: Some(TextDocumentSyncCapability::Kind(TextDocumentSyncKind::FULL)),
         semantic_tokens_provider: Some(SemanticTokensServerCapabilities::SemanticTokensOptions(
             SemanticTokensOptions {
-                work_done_progress_options: Default::default(),
                 legend: SemanticTokensLegend {
                     token_types: semantic_token_types(),
                     token_modifiers: semantic_token_modifiers(),
                 },
+                // range: Some(true), // TODO: Implement `semantic_tokens_range`
                 ..Default::default()
             },
         )),
         ..Default::default()
     }
+}
+
+/// Document.
+#[derive(Debug)]
+pub struct Document {
+    text: Rope,
+    tokens: Vec<POSToken>,
+    version: i32,
 }
 
 #[cfg(test)]
--- natural_syntax_ls/src/tests.rs
+++ natural_syntax_ls/src/tests.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 use super::*;
 
 use insta::assert_debug_snapshot;
@@ -16,6 +18,63 @@ fn token_modifiers() {
     init_tracing();
     let modifiers = semantic_token_modifiers();
     assert_debug_snapshot!(modifiers);
+}
+
+#[test]
+fn filtering_tokens() {
+    assert!(filter_token(&dummy_token("word", 1.)));
+    assert!(filter_token(&dummy_token("word,", 0.4)));
+    assert!(!filter_token(&dummy_token("word", 0.3)));
+    assert!(!filter_token(&dummy_token(",", 0.3)));
+    assert!(!filter_token(&dummy_token(",", 1.)));
+    assert!(!filter_token(&dummy_token("]", 1.)));
+    assert!(!filter_token(&dummy_token("(", 1.)));
+}
+
+fn dummy_token(word: &str, score: f64) -> POSToken {
+    POSToken {
+        word: word.into(),
+        score,
+        ..Default::default()
+    }
+}
+
+#[test]
+fn convert_tokens() {
+    init_tracing();
+    let start = Instant::now();
+    let model = POSModel::try_default().unwrap();
+    info!("Took {}ms to load the model.", start.elapsed().as_millis());
+    let input = "The easiest way of getting started is the [rustler Elixir library](https://hex.pm/packages/rustler).
+
+- Add the [rustler Elixir library](https://hex.pm/packages/rustler) as a
+  dependency of your project.
+- Run `mix rustler.new` to generate a new NIF in your project. Follow the
+  instructions.
+- If you are already using [`serde`](https://serde.rs) and/or have been using
+  `serde_rustler` before, please enable the `serde` feature in your NIF crate's
+  `Cargo.toml` on the `rustler` dependency.
+";
+    let start = Instant::now();
+    let output = model.predict(input);
+    info!("Took {}ms to predict.", start.elapsed().as_millis());
+    let mut tokens = output
+        .map(|r| r.unwrap())
+        .filter(filter_token)
+        .collect::<Vec<_>>();
+    round_scores(&mut tokens);
+    let text = Rope::from_str(input);
+    let semantic_tokens = semantic_tokens(&text, &tokens);
+    let combined = tokens.into_iter().zip(semantic_tokens).collect::<Vec<_>>();
+    assert_debug_snapshot!(combined);
+}
+
+const PRECISION: f64 = 1e-3;
+
+fn round_scores(predictions: &mut [POSToken]) {
+    predictions
+        .iter_mut()
+        .for_each(|token| token.score = (token.score / PRECISION).round() * PRECISION)
 }
 
 fn init_tracing() {
